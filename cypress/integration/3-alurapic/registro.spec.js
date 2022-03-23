3b53111dd3b1eb8cc6fb7d70a0d6f6d045f3f3a7
Gabrieli Martins <martins.gabrieli.07.almeida@outlook.com>
2022-03-22 23:46:45 -0300

    Feito a validação dos inputs

--- cypress/integration/3-alurapic/registro.spec.js
+++ cypress/integration/3-alurapic/registro.spec.js
@@ -14,4 +14,19 @@ describe('Login e registro de usuarios', () => {
     cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
 
   })
+
+  it('Verifica mensagens de email invalido', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="email"]').type('Gabrieli'); //vai digitar dentro do input qualquer coisa diferente de um email valido
+    cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible');
+  })
+
+  it('Verifica mensagens de senha com menos de 8 caracteres', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.get('input[formcontrolname="password"]').type('1234');
+    cy.contains('button', 'Register').click(); //para validar que saiu do campo e de fato ver o que esta acontecendo
+    cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible');
+  })
 })
