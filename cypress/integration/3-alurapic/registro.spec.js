1f11f6c718c45d380da0a5d7c00ce50f541d966d
Gabrieli Martins <martins.gabrieli.07.almeida@outlook.com>
2022-03-22 23:14:54 -0300

    Foi feito um caso de teste para registre-se, testando as msg

--- cypress/integration/3-alurapic/registro.spec.js
+++ cypress/integration/3-alurapic/registro.spec.js
@@ -2,7 +2,16 @@ describe('Login e registro de usuarios', () => {
   beforeEach(() => {
     cy.visit('https://alura-fotos.herokuapp.com')
   })
+  cy.contains
   it('Verifica mensagens validacao', () => {
+    cy.contains('a', 'Register now').click();
+    cy.contains('button', 'Register').click();
+    cy.contains('ap-vmessage', 'Email is required!').should('be.visible'); //quer dizer que precisa estar visivel
     
+    cy.contains('button', 'Register').click();
+    cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+    cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+    cy.contains('ap-vmessage', 'Full name is required!').should('be.visible');
+
   })
 })
